192610aab9fc61691e01c241fcdc5770d543f6d6
crs <chenrenshun@uworks.cc>
2017-01-04 16:33:23 +0800

    2.0版本

--- modules/config.js
+++ modules/config.js
@@ -2,7 +2,8 @@
 // 接口地址
 var api_url = "http://test.jwxba.com/index.php?m=API&c=User&api_ret=jsonp&a=";
 var action = window.location.href.replace(/(.*\/)*([^.]+).*/ig,"$2");
-var module = 'template';
+//var module = ["vue","interface","artDialog"];
+
 require.config({
 	baseUrl: "../",
 	paths:{
@@ -12,8 +13,9 @@ require.config({
 		 //外部插件分装
         "jquery": "plugin/jquery/1.9.1/jquery.min",
         "swiper": "plugin/swiper/3.3.1/swiper.min",
-        "template": "plugin/template/1.0/template",
+        "vue": "plugin/vue/2.0.5/vue.min",
         "dropload": "plugin/dropload/1.0/dropload",
+        "artDialog": "plugin/artDialog/artDialog",
 
         //独立分装
         "ajax": "modules/custom/interface/1.0/ajax_jsonp",   //
@@ -22,9 +24,13 @@ require.config({
         "pageClass":"modules/custom/libs/pageClass",//分页类
 	},
     shim : {
-        'index': ['jquery','ajax','common','interface']
+        'index': ['ajax','common'],
+    	'common':['artDialog'],
+        'artDialog':{
+            deps: ["jquery"],
+            exports: "dialog"
+        }
     }
-
 });
 
 require(['index']);
--- modules/controller/index.js
+++ modules/controller/index.js
@@ -1,8 +1,40 @@
-define([module],function(template) {
-	var list = {name:'shun'};
-	var tpl = template('t:crs',{
-		list:list
-	})
-	$("#shun ul").html(tpl);
-	page.Mobile();
+define(function(require) {
+	var interface = require('interface');
+	var vue = require('vue');
+//	var page = require('pageClass');
+
+	var list = {
+		arry : [
+			{name:'shun'},
+			{name:'shun1'},
+			{name:'shun2'}
+		],
+		num:0
+	};
+	new vue({
+		el:"#shun",
+		data:list,
+		methods:{
+			mode:function(data){
+				data++;
+				console.log(data);
+				var d = dialog({
+					title: '消息',
+					content: '风吹起的青色衣衫，夕阳里的温暖容颜，你比以前更加美丽，像盛开的花<br>——许巍《难忘的一天》',
+					okValue: '确 定',
+					ok: function () {
+						this.title('提交中..');
+						return false;
+					},
+					cancelValue: '取消',
+					cancel: function () {
+						d.close().remove();
+					}
+				});
+
+			d.showModal();
+			}
+		}
+	});
+//	page.Mobile();
 });
